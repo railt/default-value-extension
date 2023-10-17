996069ca8c9426717054cf570cc3d8470690d4fe
SerafimArts <nesk@xakep.ru>
2023-10-17 23:22:05 +0000

    Add default value extension to the separate repository

---
+++ tests/Unit/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Railt\Extension\DefaultValue\Tests\Unit;
+
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+#[Group('railt/default-value-extension')]
+abstract class TestCase extends BaseTestCase {}
